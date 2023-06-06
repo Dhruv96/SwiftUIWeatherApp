969f5ab2562ac9faa54392500af16a488b8bb265
Dhruv Grover <dhruvgrover@Dhruvs-MacBook-Pro.local>
2023-06-06 19:55:24 +0530

    Current Location button functionality implemented

--- SwiftUIWeather/Helpers/LocationManager.swift
+++ SwiftUIWeather/Helpers/LocationManager.swift
@@ -8,27 +8,75 @@
 import Foundation
 import CoreLocation
 
-class LocationManager: ObservableObject {
-    
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
     @Published var city: String = ""
     
-    func searchCity(text: String) {
-        guard !text.isEmpty else {
+    override init()  {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+            
+        case .notDetermined:
+            print("Not Determined")
+        case .restricted:
+            print("Restricted")
+        case .denied:
+            print("Denied")
+        case .authorizedAlways:
+            print("Authorized Always")
+        case .authorizedWhenInUse:
+            print("Authorized When in use")
+        @unknown default:
+            print("default")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            print("Error fetching locations...Location Nil")
             return
         }
         
-        let geoCoder = CLGeocoder()
-        let location = text
-        geoCoder.geocodeAddressString(location) { (placemark, error) in
-            if error != nil {
-                print(error!)
+        getUserCity(for: location) { [weak self] placemark in
+            if let placemark = placemark, let locality = placemark.locality {
+                print(locality)
+                self?.city = locality
             }
-            if placemark != nil {
-               print(placemark)
-               
-            } else {
-                print("Empty placemark")
+            else {
+                print("Something wrong")
             }
+            self?.locationManager.stopUpdatingLocation()
+        }
+    }
+}
+
+extension LocationManager {
+    func getUserCity(for location: CLLocation, completion: @escaping (CLPlacemark?) -> ()) {
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            guard error == nil else {
+                print(error)
+                completion(nil)
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                print("placemark is nil")
+                completion(nil)
+                return
+            }
+            
+            completion(placemark)
+            
         }
     }
 }
